55566cf48a40d169432fe33ff7470d81c3bb70b9
a84236416 <54300103+a84236416@users.noreply.github.com>
2019-08-20 22:06:44 +0800

    Add files via upload

---
+++ user.java
@@ -0,0 +1,14 @@
+import HelloWork.Studenttest;
+import HelloWork.Test;
+
+public class user {
+  public void method(){
+
+  }
+
+
+    public static void main(String[] args) {
+        Studenttest studenttest=new Studenttest();
+        studenttest.start();
+}
+}
